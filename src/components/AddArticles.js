84337a4198467043ceba926cd9fa16e2f30f40ee
Joshua Peavler <joshuaapeavler@gmail.com>
2020-04-14 17:04:57 -0600

    Initial commit of React-Articles project

---
+++ src/components/AddArticles.js
@@ -0,0 +1,85 @@
+import React from 'react';
+//import {Link} from 'react-router-dom';
+
+class AddArticles extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            articles: []
+        }
+
+        this.addArticle = this.addArticle.bind(this);
+        this.handleTitle = this.handleTitle.bind(this);
+        this.handleAuthor = this.handleAuthor.bind(this);
+        this.handleLink = this.handleLink.bind(this);
+        this.handleDesc = this.handleDesc.bind(this);
+        this.handleTopic1 = this.handleTopic1.bind(this);
+        this.handleTopic2 = this.handleTopic2.bind(this);
+        this.handleTopic3 = this.handleTopic3.bind(this);
+    }
+    addArticle(event) {
+        event.preventDefault();
+        let subData = [{
+            'title': this.state.title,
+            'author': this.state.author,
+            'link': this.state.link,
+            'desc': this.state.desc,
+            'topics': [
+                this.state.topic1,
+                this.state.topic2,
+                this.state.topic3
+            ]
+        }]
+        fetch('http://localhost:4000/api/articles', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : "application/json"
+            },
+            body: JSON.stringify(subData)
+        }).then(response => response.json())
+        .then(articles => this.setState({articles}))
+        .then(this.props.getArticles)
+    }
+
+    handleTitle(event) {
+        this.setState({title: event.target.value});
+    }
+    handleAuthor(event) {
+        this.setState({author: event.target.value});
+    }
+    handleLink(event) {
+        this.setState({link: event.target.value});
+    }
+    handleDesc(event) {
+        this.setState({desc: event.target.value});
+    }
+    handleTopic1(event) {
+        this.setState({topic1: event.target.value});
+    }
+    handleTopic2(event) {
+        this.setState({topic2: event.target.value});
+    }
+    handleTopic3(event) {
+        this.setState({topic3: event.target.value});
+    }
+    
+    render(){
+        return (
+            <>
+                <form onSubmit={this.addArticle}>
+                    <input placeholder="Title" onChange={this.handleTitle} required/>
+                    <input placeholder="Author" onChange={this.handleAuthor}/>
+                    <input placeholder="Link" onChange={this.handleLink} required/>
+                    <input placeholder="Desc" onChange={this.handleDesc}/>
+                    <input placeholder="Topic 1" onChange={this.handleTopic1}/>
+                    <input placeholder="Topic 2" onChange={this.handleTopic2}/>
+                    <input placeholder="Topic 3" onChange={this.handleTopic3}/>
+                    <input type="submit" value="Add Article"/>
+                </form>
+            </>
+        )
+    }
+
+}
+
+export default AddArticles;
---
+++ src/components/Articles.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import AddArticles from './AddArticles'
+
+class Articles extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            articles: []
+        }
+        this.getArticles = this.getArticles.bind(this);
+    }
+    getArticles() {
+        fetch('http://localhost:4000/api/articles')
+            .then(response => response.json())
+            .then(articles => this.setState({articles}))
+    }
+
+    componentDidMount(){
+        this.getArticles();
+    }
+    
+    render(){
+        const ArticleComponents = this.state.articles.map(article => <li>{article.title}</li>)
+        return (
+            <>
+                <h2>Articles</h2>
+                <button onClick={this.getArticles}>Refresh Articles</button>
+                <ul>
+                    {ArticleComponents}
+                </ul>
+                <h2>Add Articles</h2>
+                <AddArticles getArticles={this.getArticles}/>
+                <Link to='/'>Home</Link>
+            </>
+        )
+    }
+
+}
+
+export default Articles;
