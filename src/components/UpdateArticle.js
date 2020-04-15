f077e287b9c6934c095148238bf956fd4197995a
Joshua Peavler <joshuaapeavler@gmail.com>
2020-04-15 12:02:26 -0600

    Added form to Update Article, still need to adjust fetch

--- src/components/UpdateArticle.js
+++ src/components/UpdateArticle.js
@@ -7,20 +7,34 @@ class UpdateArticle extends React.Component {
             id: this.props.id
         }
         this.updateArticle = this.updateArticle.bind(this);
+        this.handleId = this.handleId.bind(this);
+        this.handleKey = this.handleKey.bind(this);
+        this.handleValue = this.handleValue.bind(this);
     }
 
     updateArticle(event){
         event.preventDefault();
         fetch(`http://localhost:4000/api/articles/${this.state.id}`, {
             method: 'PATCH'
-        }).then(response => response.json())
-        .then(articles => this.setState({articles}))
-        .then(this.props.getArticles)
+        }).then(this.props.getArticles)
+    }
+
+    handleId(event){
+        this.setState({id: event.target.value});
+    }
+    handleKey(event){
+        this.setState({key: event.target.value});
+    }
+    handleValue(event){
+        this.setState({value: event.target.value});
     }
     render(){
         return(
-            <form>
-
+            <form onSubmit={this.updateArticle}>
+                <input placeholder="Article id" onChange={this.handleId} required/>
+                <input placeholder="Value to Change" onChange={this.handleKey} required/>
+                <input placeholder="Change Value to" onChange={this.handleValue} required/>
+                <input type="submit" value="Update Article"/>
             </form>
         )
     }
