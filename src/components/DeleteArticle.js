1b506c1429e82c3fb3d2ca0ac6fa1149f4299280
Joshua Peavler <joshuaapeavler@gmail.com>
2020-04-15 11:26:37 -0600

    simplified actions after fetch to decrease repetetion

--- src/components/DeleteArticle.js
+++ src/components/DeleteArticle.js
@@ -13,9 +13,7 @@ class DeleteArticle extends React.Component {
         event.preventDefault();
         fetch(`http://localhost:4000/api/articles/${this.state.id}`, {
             method: 'DELETE'
-        }).then(response => response.json())
-        .then(articles => this.setState({articles}))
-        .then(this.props.getArticles)
+        }).then(this.props.getArticles)
     }
     render(){
         return(
